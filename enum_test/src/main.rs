b5ddeac68a2fdc9d9089a4a92ae232125c8718d0
NAGARRO\lalitrana <lalit.rana@nagarro.com>
2022-09-26 23:34:26 +0530

    ch 6

---
+++ enum_test/src/main.rs
@@ -0,0 +1,56 @@
+// fn main() {
+//     enum IpAddr {
+//         V4(u8, u8, u8, u8),
+//         V6(String),
+//     }
+
+//     let home = IpAddr::V4(127, 0, 0, 1);
+//     let loopback = IpAddr::V6(String::from("::1"));
+
+//     println!("Hello, world!");
+// }
+
+
+// #[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     Alaska,
+//     // --snip--
+// }
+
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {:?}!", state);
+//             25
+//         }
+//     }
+// }
+
+// fn main() {
+//     value_in_cents(Coin::Quarter(UsState::Alaska));
+// }
+
+
+fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+}
