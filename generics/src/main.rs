23592ea272490e7948ddd4623dbc636740d42d15
NAGARRO\lalitrana <lalit.rana@nagarro.com>
2022-10-01 16:48:57 +0530

    ch 10

---
+++ generics/src/main.rs
@@ -0,0 +1,102 @@
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+//     assert_eq!(*result, 100);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+//     assert_eq!(*result, 'y');
+// }
+
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+//     z: usize,
+// }
+
+// impl<T, U> Point<T, U> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+//     fn y(&self) -> &U {
+//         &self.y
+//     }
+// }
+
+// impl Point<f32, f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+
+// fn main() {
+//     let integer = Point{x:0, y:0.0, z:0};
+//     let float = Point{x:1, y:1.0, z:1};
+// }
+
+
+// fn main() {
+//     let string1 = String::from("long string is long");
+//     let result;
+//     {
+//         let string2 = String::from("xyz");
+//         result = longest(string1.as_str(), string2.as_str());
+//     }
+//     println!("The longest string is {}", result);
+// }
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest_with_an_announcement(
+        string1.as_str(),
+        string2,
+        "Today is someone's birthday!",
+    );
+    println!("The longest string is {}", result);
+}
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
